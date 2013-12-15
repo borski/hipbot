2b194bbf326f568a491acb91679b7242b3595b04
Michael Borohovski <borski@tinfoilsecurity.com>
2013-12-15 12:51:44 -0800

    Disabling invites

--- lib/hipbot/callbacks/invite.rb
+++ lib/hipbot/callbacks/invite.rb
@@ -2,9 +2,9 @@ module Hipbot
   module Callbacks
     class Invite < Base
       def initialize room_id, user_name, room_name, topic
-        with_room(id: room_id, name: room_name, topic: topic) do |room|
-          Hipbot.join_room(room)
-        end
+        # with_room(id: room_id, name: room_name, topic: topic) do |room|
+        #   Hipbot.join_room(room)
+        # end
       end
     end
   end
